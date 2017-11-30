0d24a275a0f54360a89bedcf17e5c0b359fbdbe8
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2017-11-30 11:49:54 -0700

    added starred and read functionality - talks to API db now

--- js/mail/Mail.controller.js
+++ js/mail/Mail.controller.js
@@ -4,7 +4,43 @@ angular.module('inboxApp')
 function messages($http) {
   const vm = this;
 
-  $http.get('https://floating-refuge-12080.herokuapp.com/api/messages').then(function(response){
+  $http.get('https://floating-refuge-12080.herokuapp.com/api/messages')
+  .then(function(response){
     vm.mail = response.data._embedded.messages
   })
+
+  vm.updateStarred = function(id, boolean) {
+    let data = {
+      "messageIds": [ id ],
+      "command": "star",
+      "star": boolean
+    }
+
+    $http.patch('https://floating-refuge-12080.herokuapp.com/api/messages', data).then(function(response) {
+      console.log('star patch is done!');
+
+      $http.get('https://floating-refuge-12080.herokuapp.com/api/messages')
+      .then(function(response){
+        vm.mail = response.data._embedded.messages
+      })
+    })
+  }
+
+  vm.updateRead = function(id, boolean) {
+    let data = {
+      "messageIds": [ id ],
+      "command": "read",
+      "read": boolean
+    }
+    
+    $http.patch('https://floating-refuge-12080.herokuapp.com/api/messages', data).then(function(response) {
+      console.log('read patch is done!');
+
+      $http.get('https://floating-refuge-12080.herokuapp.com/api/messages')
+      .then(function(response){
+        vm.mail = response.data._embedded.messages
+      })
+    })
+  }
+
 }
